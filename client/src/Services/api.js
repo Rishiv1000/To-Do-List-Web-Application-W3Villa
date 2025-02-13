3c2b6b5fd57b63adbc82301433d115e2e69c51a4
RISHI VERMA <114014651+Rishiv1000@users.noreply.github.com>
2025-02-13 22:18:01 +0530

    Update api.js

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "https://to-do-list-web-application-mern.vercel.app";
 // api request to register a new user
 export const registerUser = async (userData) => {
   try {
