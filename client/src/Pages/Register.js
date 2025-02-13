57bed12ae80862facb0dc8494adc145fe7bd17b8
RISHI VERMA <114014651+Rishiv1000@users.noreply.github.com>
2025-02-13 21:11:36 +0530

    Update Register.js

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Avatar from "../components/User/Avatar";
+
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser, setUserId, setToken, setPicture } from "../Store/userSlice";
