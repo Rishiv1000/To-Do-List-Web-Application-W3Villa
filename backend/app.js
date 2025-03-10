7919e1e2039fe0ba6bae9b9544c54e0e6cbfd53a
RISHI VERMA <114014651+Rishiv1000@users.noreply.github.com>
2025-03-10 09:11:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/user');
-const todoRoutes = require('./routes/todo');  // Import your todo routes
+const todoRoutes = require('./routes/todo'); 
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use(cors());
 
-// Routes
-app.use('/api/v1', authRoutes);  // Auth routes still remain under v1
-app.use('/api/v2', todoRoutes);  // Todo routes are now under v2
 
-// Connect to MongoDB
+app.use('/api/v1', authRoutes); 
+app.use('/api/v2', todoRoutes); 
+
+
 mongoose
   .connect("mongodb+srv://Rishiv:1221@cluster0.wux8rdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
